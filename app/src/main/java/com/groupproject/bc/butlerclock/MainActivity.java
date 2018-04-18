8dd064ffadfd8a85937b3d1e7c212f8f586196b0
bnguyen14 <32492548+bnguyen14@users.noreply.github.com>
2018-04-18 09:59:33 -0400

    Add files via upload

--- app/src/main/java/com/groupproject/bc/butlerclock/MainActivity.java
+++ app/src/main/java/com/groupproject/bc/butlerclock/MainActivity.java
@@ -1,37 +1,24 @@
 package com.groupproject.bc.butlerclock;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.SystemClock;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
-    final static private long ONE_SECOND = 1000;
-    final static private long TWO_SECONDS = ONE_SECOND * 2;
-    PendingIntent pi;
-    BroadcastReceiver br;
-    AlarmManager am;
-    Button button;
-    String hour, minute;
-    TextView alarms_textView;
+    private AlarmData DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        DB = new AlarmData(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        alarms_textView = findViewById(R.id.alarms_textView);
-        //Display the clock
+        TextView oncoming = findViewById(R.id.textView);
+        Cursor c = getData();
+        oncoming.setText(showData(c));
         Thread t = new Thread(){
             @Override
             public void run() {
@@ -54,44 +41,27 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         t.start();
+    }
 
-        //Test alarm button
-        startAlert();
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                am.set( AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + TWO_SECONDS, pi );
-                Toasty();
+    private String showData(Cursor c) {
+        String output = "";
+        for(int i = 0; i<3; i++){
+            if(c.moveToNext()){
+                output += c.getString(0) + "\n";
             }
-        });
+        }
+        return output;
+    }
 
-        //Get data from addAlarm.java
-        Intent intent = getIntent();
-        hour = intent.getStringExtra(addAlarm.EXTRA_message);
-        minute = intent.getStringExtra(addAlarm.EXTRA_message);
-
-        alarms_textView.setText(hour+":"+minute);
+    private Cursor getData() {
+        SQLiteDatabase db = DB.getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM alarmtime",null);
+        return c;
     }
 
     public void call_addAlarm(View view){
         Intent intent = new Intent(this,addAlarm.class );
         startActivity(intent);
-    }
-
-    private void startAlert() {
-        br = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                Toast.makeText(context, "Alarm", Toast.LENGTH_LONG).show();
-            }
-        };
-        registerReceiver(br, new IntentFilter("com.groupproject.bc.butlerclock"));
-        pi = PendingIntent.getBroadcast(this,0,new Intent("com.groupproject.bc.butlerclock"),0);
-        am = (AlarmManager) (this.getSystemService(Context.ALARM_SERVICE));
-    }
-
-    public void Toasty(){
-        Toast.makeText(this, "Alarm with start in " + TWO_SECONDS/1000 + " seconds.", Toast.LENGTH_SHORT).show();
     }
 
 }
